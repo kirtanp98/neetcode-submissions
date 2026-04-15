3528601299029e9514d26ecf03fd9dfa858c965c
Kirtan Patel <kirtanpatl@gmail.com>
2026-04-15 10:58:16 -0400

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+
+
+        def helper(op, close, subset):
+            if op == n and close == n:
+                result.append(subset)
+
+            if op <= n:
+                subset += "("
+                helper(op +1, close, subset)
+                subset = subset[:-1]
+            if close <= n and op > close:
+                subset += ")"
+                helper(op, close+1, subset)
+                subset = subset[:-1]
+
+        helper(0,0, "")
+
+        return result
