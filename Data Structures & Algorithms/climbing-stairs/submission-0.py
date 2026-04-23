d32ca8c5ee5ccff728a0116d47c7ec7a49ae3661
Kirtan Patel <kirtanpatl@gmail.com>
2026-04-23 13:32:21 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        def rec(i):
+            if i >= n:
+                return i == n
+
+            if i in memo:
+                return memo[i]
+            memo[i] = rec(i+1) + rec(i+2)
+            return memo[i]
+        
+        return rec(0)
